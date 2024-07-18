7a48977648cd81f5e7d8b3db4d285fc128e6f5f1
BeyzCoder <stevenbaes11@yahoo.com>
2024-07-18 02:20:40 -0600

    Added market contact section and google review section.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Header from "./components/Header";
 import Landing from "./components/Landing";
 import Feature from "./components/Feature";
 import Showcase from "./components/Showcase";
+import Market from "./components/Market";
 
 function App() {
   return (
@@ -10,6 +11,10 @@ function App() {
       <Landing />
       <Feature />
       <Showcase />
+      <Market />
+      <div class="review-section">
+        <div class="elfsight-app-b316b523-850a-4557-b02e-1e81b22bcb2e" data-elfsight-app-lazy></div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Market.js
@@ -0,0 +1,24 @@
+import "../styles/market.css";
+
+const Market = () => {
+    return (
+        <section className="market-section">
+            <div className="glassy-black">
+                <div className="tagline">
+                    <h3>What are you waiting for make an appointment now!</h3>
+                </div>
+                <div className="divider-con">
+                    <div className="address">
+                        <span>802 Gray Avenue, Saskatoon, SK S7N 2J4</span>
+                    </div>
+                    <div className="contact-details">
+                        <span>306 - 315 - 5880</span>
+                        <span>802windowcleaning@gmail.com</span>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+}
+
+export default Market;
